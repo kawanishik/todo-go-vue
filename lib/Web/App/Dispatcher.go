301496e8e046ec899c08dfb30231dbfafc1f001e
kawanishik <kk.yuzio19@gmail.com>
2024-05-19 17:39:56 +0900

    CORSエラー回避のmiddlewareの追加

--- lib/Web/App/Dispatcher.go
+++ lib/Web/App/Dispatcher.go
@@ -7,8 +7,18 @@ import (
     "github.com/gin-gonic/gin"
 )
 
+func CORSMiddleware() gin.HandlerFunc {
+    return func(c *gin.Context) {
+        c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+        c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+        c.Writer.Header().Set("Access-Control-Allow-Headers", "*")
+        c.Next()
+    }
+}
+
 func HandleRoutes() {
     dispatcher := gin.Default()
+    dispatcher.Use(CORSMiddleware()) // CORSミドルウェアを適用
     dispatcher.GET("/api/index", service_todo.Index)
     dispatcher.POST("/api/create", service_todo.Create)
     dispatcher.DELETE("/api/delete", service_todo.Delete)
