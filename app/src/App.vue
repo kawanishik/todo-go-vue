ab80ff63c695dab9730cd8d78a467a1486f41a19
kawanishik <kk.yuzio19@gmail.com>
2024-05-24 01:21:47 +0900

    基本的なグローバルヘッダーの作成

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,23 +1,12 @@
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div class="todo-app">
+    <global-header />
+  </div>
 </template>
 
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import GlobalHeader from '@/components/presentationals/GlobalHeader.vue';
+
 import type { Todo } from "@/types/todo";
 
 import TodoRepository from "@/repositories/todo";
@@ -27,66 +16,12 @@ const todoStore = useTodoStore();
 TodoRepository.getTodoList().then((res: Todo[] | null) => todoStore.setTodoList(res));
 </script>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+<style lang="scss" scoped>
+@import '@/assets/scss/color.scss';
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.todo-app {
+  height: 100vh;
+  width: 100vw;
+  background-color: $color-bg;
 }
 </style>
---
+++ app/src/components/presentationals/GlobalHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="title">Todo List</div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style lang="scss" scoped>
+.title {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: 'Caveat', cursive;
+  height: 100px;
+  font-size: 40px;
+}
+</style>
